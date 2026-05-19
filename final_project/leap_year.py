98c0611a014c74b16dc6be626843911683a58ae4
Kristian Hernandez <hkristianlloyd2@gmail.com>
2026-05-19 11:46:49 +0800

    add leap year project - not finalize

---
+++ final_project/leap_year.py
@@ -0,0 +1,11 @@
+while True:
+    year = int(input("Input a year: "))
+
+    if year % 400 == 0:
+        print("Leap Year")
+    elif year % 100 == 0:
+        print("It is a common year")
+    elif year % 4 == 0:
+        print("Leap Year")
+    else:
+        print("It is a common year")
